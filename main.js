98c8c24063d500f0528c9a431bd7c5b123f80f31
joaooknerevangelista-jpg <joao.okner.evangelista@escola.pr.gov.br>
2026-04-30 07:55:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2027-09-07T00:00:00");
 const tempoObjetivo2 = new Date("2027-01-20T00:00:00");
 const tempoObjetivo3 = new Date("2030-01-15T00:00:00");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempoObjetivo4 = new Date("2040-02-10T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
